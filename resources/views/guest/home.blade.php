785dfc2ff8c51fd4a4317857540ba032948a1ba1
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-07-20 17:22:00 +0200

    add db whit movies list

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Homepage')
+@dd($movies)
+@section('main-content')
+    <h1>
+        Homepage
+    </h1>
+
+    
+@endsection
--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Welcome')
-
-@section('main-content')
-    <h1 class="text-center m-5">
-        Benvenuto/a !!!!!
-    </h1>
-@endsection
